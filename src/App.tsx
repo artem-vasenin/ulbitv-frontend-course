2a0415567b28e637871f46c4ae0577bee2de7db5
Rusich <artem-skobar@yandex.ru>
2023-05-05 00:06:24 +0300

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 
 import { MainPage } from './pages';
+import { useTheme } from './theme/useTheme';
 import { AboutPageAsync } from './pages/About';
 import { CounterPageAsync } from './pages/Counter';
-import './index.scss';
+import './styles/index.scss';
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
-      <ul>
-        <li>
-          <Link to={'/'}>Home</Link>
-        </li>
-        <li>
-          <Link to={'/about'}>About</Link>
-        </li>
-        <li>
-          <Link to={'counter'}>Counter</Link>
-        </li>
-      </ul>
+    <div className={`app ${theme}`}>
+      <div>
+        <Link to={'/'}>Home</Link>
+      </div>
+      <div>
+        <Link to={'/about'}>About</Link>
+      </div>
+      <div>
+        <Link to={'counter'}>Counter</Link>
+      </div>
+
+      <button onClick={toggleTheme}>Theme</button>
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
---
+++ src/theme/useTheme.ts
@@ -0,0 +1,19 @@
+import { LOCAL_STORAGE_THEME_KEY, ThemeContext, Themes } from './ThemeContext';
+import { useContext } from 'react';
+
+interface UseThemeResult {
+  toggleTheme: () => void;
+  theme: Themes;
+}
+
+export const useTheme = (): UseThemeResult => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    const newTheme = theme === Themes.LIGHT ? Themes.DARK : Themes.LIGHT;
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+    setTheme(newTheme);
+  }
+
+  return { theme, toggleTheme };
+}
