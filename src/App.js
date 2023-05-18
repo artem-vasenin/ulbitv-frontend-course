1500f85290a573c5997688ecf6a79c22e26ebc07
Rusich <artem-skobar@yandex.ru>
2023-05-18 20:25:25 +0300

    added user event tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import {useState} from "react";
+
 import './App.css';
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
-      <h1>Hello Rusich</h1>
-      <button>Click</button>
-      <input type="text" placeholder="password"/>
+      <h1 data-testid="title">Hello Rusich</h1>
+      <p data-testid="text">{input}</p>
+      {toggle ? <div data-testid="toggle">Toggle div</div> : null}
+      <button data-testid="button" onClick={() => setToggle(!toggle)}>Click</button>
+      <input data-testid="input" type="text" onChange={e => setInput(e.target.value)}/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
 describe('Test App', () => {
-  test('renders react title', () => {
+  test('Проверка наличия элементов на странице', () => {
     render(<App />);
-    const titleElement = screen.getByText(/hello rusich/i);
-    const btnElement = screen.getByRole('button');
-    const inputElement = screen.getByPlaceholderText(/password/i);
-    expect(titleElement).toBeInTheDocument();
-    expect(btnElement).toBeInTheDocument();
-    expect(inputElement).toBeInTheDocument();
-    expect(inputElement).toMatchSnapshot();
+    const title = screen.getByTestId('title');
+    const btn = screen.getByTestId('button');
+    const input = screen.getByTestId('input');
+    expect(title).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+  });
+  test('Проверка нажатия кнопки и появление элемнета toggle', () => {
+    render(<App />);
+    const btn = screen.getByTestId('button');
+    expect(screen.queryByTestId('toggle')).toBeNull();
+    expect(btn).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle')).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle')).toBeNull();
+  });
+  test('проверка инпута', () => {
+    render(<App />);
+    const input = screen.getByTestId('input');
+    expect(input).toBeInTheDocument();
+    expect(screen.getByTestId('text')).toContainHTML('');
+    // fireEvent.input(input, { target: { value: 'some text' } });
+    userEvent.type(input, 'some text');
+    expect(screen.getByTestId('text')).toContainHTML('some text2');
   });
 });
