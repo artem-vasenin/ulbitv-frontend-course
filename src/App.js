9bb1bdbd9f7d2b8e6ae9dd715c3ed57ba2c85092
Rusich <artem-skobar@yandex.ru>
2023-05-21 22:16:02 +0300

    added base redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import {useDispatch, useSelector} from "react-redux";
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+  const [val, setVal] = useState();
+
+  const checkValue = () => {
+    return val && Number.isInteger(+val);
+  };
+
+  const addCash = () => {
+    checkValue() && dispatch({ type: 'ADD_CASH', payload: +val });
+  };
+
+  const getCash = () => {
+    checkValue() && dispatch({ type: 'GET_CASH', payload: +val });
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>На счету: {cash || 0} руб.</h1>
+      <div>
+        <div>
+          <input onChange={e => setVal(e.target.value)} />
+        </div>
+        <button onClick={addCash}>Пополнить на 10р</button>
+        <button onClick={getCash}>Снять 10р</button>
+      </div>
     </div>
   );
 }
