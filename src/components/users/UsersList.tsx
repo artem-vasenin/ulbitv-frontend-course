cf509b8f02857ef1b031b798369eb8f5929cd17b
Rusich <artem-skobar@yandex.ru>
2023-05-23 19:19:57 +0300

    added async users fetching

--- src/components/users/UsersList.tsx
+++ src/components/users/UsersList.tsx
@@ -1,13 +1,32 @@
-import React, {FC} from 'react';
+import React, { FC, useEffect } from 'react';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { useDispatch } from 'react-redux';
+import { getUsers } from '../../store/actions/user';
 
 export const UsersList: FC = () => {
-  const {list} = useTypedSelector(state => state.users);
-  console.log(list);
+  const dispatch = useDispatch();
+  const {loading, list, error} = useTypedSelector(state => state.users);
+
+  useEffect(() => {
+    // @ts-ignore
+    dispatch(getUsers());
+  }, []);
+
+  if (loading) {
+    return <h3>Загрузка...</h3>
+  }
+
+  if (error) {
+    return <h3>{error}</h3>
+  }
+
+  if (!error && !loading && !list.length) {
+    return <h3>Пользователи не найдены</h3>
+  }
 
   return (
     <section>
-      Hello List
+      <ol>{list.map(u => <li key={u.id}>{u.name}</li>)}</ol>
     </section>
   );
 };
